1ada101a230537b65fa6e06a6a5eb6c14511f358
enamahmedshahaz <enamahmedshahaz@gmail.com>
2023-11-09 23:52:58 +0600

    comment out some code before deploy and add vercel.json file

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+      //  await client.connect();
 
         const database = client.db("onlineGroupStudyDB");
         const assignmentCollection = database.collection("assignments");
@@ -182,10 +182,9 @@ async function run() {
             res.send(result);
         })
 
-
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        //await client.db("admin").command({ ping: 1 });
+       // console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     } finally {
         // Ensures that the client will close when you finish/error
